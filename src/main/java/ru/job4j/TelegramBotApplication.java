a601b7b9b120682c419411b4bbf182bab7324f1f
ilyashabelnik <shabelnik_ilya@mail.ru>
2024-09-27 18:05:35 +0300

    - fix checkstyle

--- src/main/java/ru/job4j/TelegramBotApplication.java
+++ src/main/java/ru/job4j/TelegramBotApplication.java
@@ -3,8 +3,7 @@ package ru.job4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class TelegramBotApplication {
-
+public class TelegramBotApplication {
 
     /**
      * Точка старта приложения.
